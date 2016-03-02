23183b5d88c821833fe8cdff20599686a23f0d97
wolfram74 <phaugen@gmail.com>
2016-03-02 17:54:32 -0600

    #new route wired up correctly

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,2 +1,9 @@
 class CohortsController < ApplicationController
+  def new
+  end
+  def create
+  end
+  def show
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  resources :cohorts, only: [:new, :create, :show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
