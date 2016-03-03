183c3354c85441d1564a25f8984a967f0d57d7d9
wolfram74 <phaugen@gmail.com>
2016-03-02 20:08:55 -0600

    event listeners wired up properly

--- app/assets/javascripts/cohorts.js
+++ app/assets/javascripts/cohorts.js
@@ -1,12 +1,37 @@
 $('document').ready(function(){
-  if(window.location.pathname.indexOf('cohorts') < 0){return}
-  $('form').on('change', cohort.formHandler )
+  if(window.location.pathname.indexOf('cohorts') < 0){return};
+  console.log('cohorts recognized')
+  $('form').on('change keyup', cohort.formHandler)
+  $('.newEntry').on('click', cohort.buttonHandler)
 })
 
 var cohort = (function(){
   var API = {}
   API.formHandler = function(event){
-    console.log(event.target)
+    var $form = $(event.target).closest('form')
+    console.log($form)
+    var formString = $form.serializeArray()
+    console.log(formString)
+    // console.log($.parseJSON(formString) )
+    // debugger
+  };
+  API.buttonHandler = function(event){
+    console.log('more things')
+    API[$(event.target).attr('id')]()
+  };
+  API.addBoot = function(){
+    var $template = $('#bootTemplate')
+    var $copy = $template.clone()
+    $copy.attr('class', '')
+    $copy.attr('id', '')
+    $('#bootList').append($copy)
+  }
+  API.addProject = function(event){
+    var $template = $('#projectTemplate')
+    var $copy = $template.clone()
+    $copy.attr('class', '')
+    $copy.attr('id', '')
+    $('#projectList').append($copy)
   }
   return API
-  })
+  })()
